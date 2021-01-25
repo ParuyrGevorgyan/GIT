4a59796175184c3cc9025fb9e8edf3886fce7cc0
ParuyrGevorgyan <gevorgyan.paruyr@gmail.com>
2021-01-25 17:10:42 +0100

    edytowano main.cpp

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+#include "./calculations/additional.hpp"
+#include "./calculations/division.hpp"
+#include "./calculations/multiplication.hpp"
+#include "./calculations/subtraction.hpp"
+
+int main()
+{
+    char operate;
+    int num1, num2, result;
+
+    cout << "Podaj pierwsza liczbe: " << endl;
+    cin >> num1;
+    return 0;
+}
+

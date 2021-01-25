70c899aa5e53f8954cc6427c19723df83dedafab
Jkoltonowski <jakmilosz@gmail.com>
2021-01-25 16:13:03 +0100

    odejmowanie i mnozenie

---
+++ calculations/multiplication.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int division()
+{
+    int mul = a * b;
+    return mul;
+}
---
+++ calculations/subtraction.cpp
@@ -0,0 +1,7 @@
+#include "subtraction.hpp"
+
+int funcSubtraction(int a, int b)
+{
+    int sum=a-b;
+    return sum;
+}

0003d3d8c5d350401877930b65dc058d860b230c
Jkoltonowski <jakmilosz@gmail.com>
2021-01-25 22:39:16 +0100

    Poprawa pierwszego programu

--- calculations/multiplication.cpp
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
-
-int division()
-{
-    int mul = a * b;
-    return mul;
-}
--- calculations/multiplication.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef multiplication_hpp
-#define multiplication_hpp
-
-#include <stdio.h>
-
-int funcMultiplication(int a, int b);
-
-#endif /* multiplication_hpp */
